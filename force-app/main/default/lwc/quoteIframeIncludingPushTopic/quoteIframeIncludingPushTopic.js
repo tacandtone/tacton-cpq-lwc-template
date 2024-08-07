7049339415997d9f8e80e46ca111ba2ef7ec52ca
tacandtone <49035078+tacandtone@users.noreply.github.com>
2024-08-07 00:01:44 -0500

    First commit

---
+++ force-app/main/default/lwc/quoteIframeIncludingPushTopic/quoteIframeIncludingPushTopic.js
@@ -0,0 +1,107 @@
+import { LightningElement,wire, api,track } from 'lwc';
+import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
+import { subscribe,unsubscribe, onError } from 'lightning/empApi';
+import { getRecord } from 'lightning/uiRecordApi';
+import { RefreshEvent } from 'lightning/refresh';
+
+
+export default class QuoteIframeIncludingPushTopic extends LightningElement {
+    @api recordId;
+    @api tactonCPQUrlEmbedded;
+    @api iframeHeight;
+    @api scrolling;
+    @track quote;
+    
+
+    @track channelQuoteLineItemName = '/topic/QuoteLineItem';
+    @track channelQuoteDocumentName = '/topic/QuoteDocument';
+    @track quoteChangeEvent = '/data/QuoteChangeEvent';
+    @track strResponse = '';
+    loading = false;
+
+
+    connectedCallback() {
+        this.loading = true;
+        this.registerErrorListener();
+        this.registerSubscribe();
+    }
+
+    disconnectedCallback() {
+        unsubscribe(this.subscription, () => console.log('Unsubscribed to change events.'));
+    }
+    
+    // Called by connectedCallback()
+    registerErrorListener() {
+        onError(error => {
+            console.error('Salesforce error', JSON.stringify(error));
+        });
+    }
+
+    registerSubscribe() {
+        const changeEventCallback = changeEvent => {
+            console.log( 'Update received : ', JSON.stringify( changeEvent ) );
+            //eval("$A.get('e.force:refreshView').fire();");
+            this.dispatchEvent(new RefreshEvent());
+        };
+
+        // Sets up subscription and callback for change events
+        subscribe(this.quoteChangeEvent, -1, changeEventCallback).then(subscription => {
+            this.subscription = subscription;
+        });
+    }
+
+    renderedCallback() {
+        
+        var contextPlug = this;
+        const spinnerContainer = this.template.querySelector('.slds-spinner_container');
+        const containerElem = this.template.querySelector('.iframe-container');
+
+        const iframe = document.createElement('iframe');
+        iframe.src = this.fullUrl(); // iFrame src; add this URL to CSP
+        iframe.id = 'tactonCPQ';
+        iframe.width = '100%';
+        
+        iframe.height = "800px";
+        if(this.iframeHeight){
+            iframe.height = this.iframeHeight;
+        }
+        iframe.scrolling =this.scrolling;
+        containerElem.appendChild(iframe);
+
+        // onload() before setting 'src'
+        iframe.onload = function(e){
+            spinnerContainer.classList.add("slds-hide"); // hide spinner
+        };
+       
+    }
+    afterLoad(e){
+        spinnerContainer.classList.add("slds-hide");
+    }
+    registerErrorListener() {
+        // Invoke onError empApi method
+        onError(error => {
+            console.log( 'Received error from server: ', JSON.stringify( error ) );
+            // Error contains the server-side error
+        });
+    }
+
+    fullUrl() {
+        var cpqUrl = this.tactonCPQUrlEmbedded;
+        console.log("url",cpqUrl);
+        if(cpqUrl==undefined||cpqUrl==""){
+            cpqUrl = "https://se-demo.admin.cpqdemo.tacton.com/!uyOHyC3L-KFGD4Ml~tickets~T-00000098/$embed/jump/trampoline/0d4a65da-ebce-46da-9a96-34bd32714698?crmSetting=Salesforce&object=<OBJECT ID>";
+        }
+
+        var recordId = this.recordId;
+        return cpqUrl.replace("<OBJECT ID>",recordId);
+    }   
+
+
+
+
+    get frameName() {
+        return 'cpqembedded' + Date.now();
+    }    
+
+    
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+Here is a quick how-to for getting started with updating the code for your org.
+ 
+1) Install VsCode
+https://code.visualstudio.com/Download
+Once installed close it.
+ 
+2) Install Salesforce DX CLI
+https://developer.salesforce.com/tools/sfdxcli
+ 
+3) Install Salesforce Extension Pack in VsCode
+Open VsCode and press "Ctrl + Shift + X" and search for "Salesforce Extension Pack" and click the "Install button"
+ 
+4) Create project
+ 
+In VsCode press "Ctrl + Shift + P" and select "SFDX: Create project with Manifest"
+Choose empty
+Give it a name
+Select where to store the code
+ 
+5) Connect project with your Salesforce Org
+Press "Ctrl + Shift + P" and select "SFDX: Authorize an Org"
+Select org type (probably Production, or if you have defined it in sfdx-project.json then "Project Default")
+Give it an alias
+ 
+This opens a web page where you login as your org user
+ 
+6) Download code
+Select manifest -> package.xml , right click and select "Retrieve this Source from Org"
+Once done you should have all components etc in your project.
